f148a3b992e6cd90399aa86f0b37fce0ead2680c
IEVN1001-21000502 <joshua200314@gmail.com>
2024-09-10 08:57:33 -0600

    intro ts

---
+++ temas-ts/01-tipos-dato.ts
@@ -0,0 +1,25 @@
+console.log("Hola Mundo")
+
+var nombre = 'juan'
+let apellido = 'perez'
+const edad = 30
+
+let num1 = 5
+let num2 = 10
+let num3 = 3
+
+let suma = num1 + num2 + num3
+
+console.log(suma)
+
+let correo:string
+let edad2:number
+let activo:boolean
+
+correo  = 'prueba@gmail.com'
+edad2 = 15
+activo = true
+
+let matricula:number|string
+matricula = 123412
+matricula = '2245A45'
